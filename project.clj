5850c666ff569575f21b7db0e746cf6be7bf1724
natto <natto@natto-tp.(none)>
2012-01-22 23:14:33 -0500

    oauth to 1.2.10-SNAPSHOT

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :decription "Clojure bindings to Dropbox API"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		         [clj-oauth "1.2.1"]]
+                 [clj-oauth "1.2.10-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
